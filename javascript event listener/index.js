3e02adf4a48395528178e1eaa2739a5a836c986b
Mahmudul Hasan Jahid <132154039+mahmudulhasanjahid@users.noreply.github.com>
2023-10-20 19:55:45 +0600

    Add files via upload

---
+++ javascript event listener/index.js
@@ -0,0 +1,11 @@
+var myVar = document.querySelector("h1");
+myVar.addEventListener("mouseover",function(){
+myVar.classList.add("my-style");
+}
+);
+
+var myVar = document.querySelector("h1");
+myVar.addEventListener("mouseout",function(){
+myVar.classList.remove("my-style");
+}
+);
